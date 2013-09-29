774e5fb16fa986531f305ff3402060693009ab7f
Vojtech Bocek <vbocek@gmail.com>
2013-09-29 20:26:37 +0200

    [0.1.3] Bump version after aef32bf72908

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #ifndef VERSION_H
 #define VERSION_H
-    #define VERSION 0x000102 // 0xMMNNPP
-    #define VERSION_STR "0.1.2"
+    #define VERSION 0x000103 // 0xMMNNPP
+    #define VERSION_STR "0.1.3"
 #endif
